f9fbf67dc5e486e525906d279addc7478890de1f
Einar Gangsø <einargangso@gmail.com>
2010-07-28 18:02:00 +0200

    Added a new theme which has almost same layout as the default apache look.

--- standalone/autoindex/header.php
+++ standalone/autoindex/header.php
@@ -20,14 +20,13 @@
 		return $result;
 	}	
 ?>
-	
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
 		<title><?php echo getPath(); ?></title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<link rel="stylesheet" href="<?php print BASE_URL ?>/reset.css" />
-		<link rel="stylesheet" href="<?php print BASE_URL ?>/main.css" />
+		<link rel="stylesheet" href="<?php print BASE_URL ?>/css/reset.css" />
+		<link rel="stylesheet" href="<?php print BASE_URL ?>/css/autoindex_simple.css" />
 	</head>
 	<body>
 		<div id="page">

e355f226538f2d74de71bd84098cabe65fa280e2
Einar Gangsø <einargangso@gmail.com>
2010-07-28 17:38:00 +0200

    updated header.php to validate xhtml 1.0 strict

--- standalone/autoindex/header.php
+++ standalone/autoindex/header.php
@@ -21,15 +21,16 @@
 	}	
 ?>
 	
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
 		<title><?php echo getPath(); ?></title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<link rel="stylesheet" href="<?php print BASE_URL ?>/reset.css" />
 		<link rel="stylesheet" href="<?php print BASE_URL ?>/main.css" />
 	</head>
 	<body>
 		<div id="page">
-			<div class="pathline"><a href="/"><img src="<?php print BASE_URL ?>/icons/home.png"></a> <?php print createFilePath();?></div>
+			<div class="pathline"><a href="/"><img src="<?php print BASE_URL ?>/icons/home.png" alt="" /></a> <?php print createFilePath();?></div>
 			
 			<div id="dirlist">
